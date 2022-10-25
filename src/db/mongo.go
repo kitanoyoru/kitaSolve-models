149bb94066d67eca0e1e838c1300c4535b83dd17
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-10-25 22:00:27 +0300

    Add connect/disconnect db functions

---
+++ src/db/mongo.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connectDB() *mongo.Client{
+  if err := godotenv.Load(); err != nil {
+    log.Println("[ERROR] Env file doesn't exists")
+  }
+
+  uri := os.Getenv("MONGODB_URI")
+  if uri == "" {
+    log.Fatal("[ERROR] MongoDB uri doesn't correct")
+  }
+
+  client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+  if err != nil {
+    panic(err)
+  }
+  
+  return client 
+}
+
+func disconnectDB(client *mongo.Client) {
+  if err := client.Disconnect(context.TODO()); err != nil {
+    panic(err)
+  }
+}
+
+
