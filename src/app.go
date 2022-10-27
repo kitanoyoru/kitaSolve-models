dc81f717d4ce603f2c8e4899fdd9edb50ae03098
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-10-27 21:58:33 +0300

    Finished first mark of grpc server struct

--- src/app.go
+++ src/app.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+
 	pb "kitasolve/models/grpc"
-  "kitasolve/models/http"
+	sv "kitasolve/models/http"
+	
+  "google.golang.org/grpc"
+)
+
+const (
+  port = 5000
 )
 
 func main() {
-  fmt.Println("hello world")
+  lis, err := net.Listen("tcp", port)
+  if err != nil {
+    log.Fatalf("Failed to listen %v port: %v", port, err)
+  }
+
+  s := grpc.NewServer()
+  pb.RegisterKitaSolveModelsServer(s, &sv.Server{})
+
+  if err := s.Serve(list); err != nil {
+    log.Fatalf("Failed to serve on %v port : %v", port, err)
+  }
 }
--- src/http/server.go
+++ src/http/server.go
@@ -1,22 +1,25 @@
 package server
 
 import (
-	"fmt"
-	"log"
 	"context"
+	"fmt"
+	"io"
+	"log"
+	"strings"
 
 	pb "kitasolve/models/grpc"
-	
-  "github.com/golang/protobuf/ptypes/wrappers"
+
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 type Server struct {
-  solveMap map[string]pb.Solve // TODO: Change to db connextion
+  solveMap map[string]pb.Solve // TODO: Change to db connection
 }
 
+// Add solve to db
 func (s *Server) AddSolve(ctx context.Context, solve *pb.Solve) (*wrappers.StringValue, error) {
   s.solveMap[solve.Id] = *solve
   return &wrappers.StringValue {
@@ -24,6 +27,7 @@ func (s *Server) AddSolve(ctx context.Context, solve *pb.Solve) (*wrappers.Strin
   }, status.New(codes.OK, "").Err() 
 }
 
+// Get solve from db
 func (s *Server) GetSolve(ctx context.Context, id *wrappers.StringValue) (*pb.Solve, error) {
   if solve, ok := s.solveMap[id.Value]; ok {
     return &solve, status.New(codes.OK, "").Err()
@@ -31,5 +35,43 @@ func (s *Server) GetSolve(ctx context.Context, id *wrappers.StringValue) (*pb.So
     return nil, status.Errorf(codes.NotFound, "Solve doesn't exists: ", id)
   }
 }
+
+// Get all solves which incldue this query
+func (s *Server) SearchSolves(searchQuery *wrappers.StringValue, stream pb.KitaSolveModels_SearchSolveServer) error {
+  for _, value := range s.solveMap {    // TODO: Change to findAll mongodb method
+    if strings.Contains(value.Content, searchQuery.Value) {
+      err := stream.Send(&value)
+      if err != nil {
+        return fmt.Errorf("[ERROR] Error in sending stream output to client ")
+      }
+      break
+    }
+  }
+  return nil
+}
+
+// Update streams
+func (s *Server) UpdateSolves(stream pb.KitaSolveModels_UpdateSolveServer) error {
+  for {
+    solve, err := stream.Recv()
+    if err == io.EOF {
+      return stream.SendAndClose(&wrappers.StringValue { Value: "[200] Requested updates was processed" })
+    }
+    if err != nil {
+      return err
+    }
+
+    s.solveMap[solve.Id] = *solve // TODO: Change to updateOne mongodb method
+  }
+}
+
+
+
+
+
+
+
+
+
 
 
